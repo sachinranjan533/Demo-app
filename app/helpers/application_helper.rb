13065fdea4db909d6bb25cdf1287faa054956e18
ankit <ankit.kumar@daffodilsw.com>
2023-08-11 20:41:44 +0530

    modified layout of navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,12 @@
 module ApplicationHelper
+
   def format_date(date)
     date.strftime("%d/%m/%Y") if date
   end
+
+  def active_menu_class?(path)
+    return 'menu_active' if request.path == path
+    ''
+  end
+  
 end
